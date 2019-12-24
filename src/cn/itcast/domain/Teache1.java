9ccf1c86ced1234aaa38c454e3cd7923470c816f
MRCHEN <530059857@qq.com>
2019-12-24 09:42:56 +0800

    commit Teache1

---
+++ src/cn/itcast/domain/Teache1.java
@@ -0,0 +1,37 @@
+package cn.itcast.domain;
+
+/**
+ * @ClassName Teacher
+ * @Desciption
+ * @Author ³Â×¿ÐÅ
+ * @Date 2019/12/24 9:40
+ * @Version 1.0
+ **/
+public class Teache1 {
+    private int id;
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
